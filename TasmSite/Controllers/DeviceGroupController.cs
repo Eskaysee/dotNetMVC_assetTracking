438b594c55605f2db1489ee56b09d91f62a2b583
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-02-26 01:31:32 +0200

    created Controllers and generated views

---
+++ TasmSite/Controllers/DeviceGroupController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TasmSite.Models;
+
+namespace TasmSite.Controllers
+{
+    public class DeviceGroupController : Controller
+    {
+        private TasmEntities db = new TasmEntities();
+
+        // GET: DeviceGroup
+        public ActionResult Index()
+        {
+            var deviceGroups = db.DeviceGroups.Include(d => d.DeviceGroup2);
+            return View(deviceGroups.ToList());
+        }
+
+        // GET: DeviceGroup/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DeviceGroup deviceGroup = db.DeviceGroups.Find(id);
+            if (deviceGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deviceGroup);
+        }
+
+        // GET: DeviceGroup/Create
+        public ActionResult Create()
+        {
+            ViewBag.ParentGroupId = new SelectList(db.DeviceGroups, "Id", "Name");
+            return View();
+        }
+
+        // POST: DeviceGroup/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,ParentGroupId")] DeviceGroup deviceGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DeviceGroups.Add(deviceGroup);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ParentGroupId = new SelectList(db.DeviceGroups, "Id", "Name", deviceGroup.ParentGroupId);
+            return View(deviceGroup);
+        }
+
+        // GET: DeviceGroup/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DeviceGroup deviceGroup = db.DeviceGroups.Find(id);
+            if (deviceGroup == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ParentGroupId = new SelectList(db.DeviceGroups, "Id", "Name", deviceGroup.ParentGroupId);
+            return View(deviceGroup);
+        }
+
+        // POST: DeviceGroup/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,ParentGroupId")] DeviceGroup deviceGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(deviceGroup).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ParentGroupId = new SelectList(db.DeviceGroups, "Id", "Name", deviceGroup.ParentGroupId);
+            return View(deviceGroup);
+        }
+
+        // GET: DeviceGroup/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DeviceGroup deviceGroup = db.DeviceGroups.Find(id);
+            if (deviceGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deviceGroup);
+        }
+
+        // POST: DeviceGroup/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DeviceGroup deviceGroup = db.DeviceGroups.Find(id);
+            db.DeviceGroups.Remove(deviceGroup);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TasmSite/Controllers/FirmwareController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TasmSite.Models;
+
+namespace TasmSite.Controllers
+{
+    public class FirmwareController : Controller
+    {
+        private TasmEntities db = new TasmEntities();
+
+        // GET: Firmware
+        public ActionResult Index()
+        {
+            return View(db.Firmwares.ToList());
+        }
+
+        // GET: Firmware/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Firmware firmware = db.Firmwares.Find(id);
+            if (firmware == null)
+            {
+                return HttpNotFound();
+            }
+            return View(firmware);
+        }
+
+        // GET: Firmware/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Firmware/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Version,ReleaseDate")] Firmware firmware)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Firmwares.Add(firmware);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(firmware);
+        }
+
+        // GET: Firmware/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Firmware firmware = db.Firmwares.Find(id);
+            if (firmware == null)
+            {
+                return HttpNotFound();
+            }
+            return View(firmware);
+        }
+
+        // POST: Firmware/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Version,ReleaseDate")] Firmware firmware)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(firmware).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(firmware);
+        }
+
+        // GET: Firmware/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Firmware firmware = db.Firmwares.Find(id);
+            if (firmware == null)
+            {
+                return HttpNotFound();
+            }
+            return View(firmware);
+        }
+
+        // POST: Firmware/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Firmware firmware = db.Firmwares.Find(id);
+            db.Firmwares.Remove(firmware);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
