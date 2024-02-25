9a1c55e779a27e5c78caf7cfb989d50618f24d84
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-02-25 15:40:34 +0200

    Wrote the script for creating the DeviceGroup table. I also renamed the Type table to DeviceType table so its clearer and to avoid syntax issues

---
+++ DeviceGroup.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DeviceGroup]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Name] VARCHAR(255) NOT NULL,
+    [ParentGroupId] INT,
+    FOREIGN KEY (ParentGroupId) REFERENCES DeviceGroup(Id)
+)
