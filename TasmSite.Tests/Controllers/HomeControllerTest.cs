4b84e139831dbeb55293f4cc0f14e0039daa07d7
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-02-27 01:26:16 +0200

    Wrote a test for the HomeController and refactored the controllers (removed all unused imports)

---
+++ TasmSite.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using TasmSite.Controllers;
+
+namespace TasmSite.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Our Journey", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Contact Information", result.ViewBag.Message);
+        }
+    }
+}
--- TasmSite/Controllers/FirmwareController.cs
+++ TasmSite/Controllers/FirmwareController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using TasmSite.Models;
 
@@ -119,7 +116,7 @@ namespace TasmSite.Controllers
             if (firmware.Firmware1.Any())
             {
                 nextId = firmware.Firmware1.ElementAt(0).Id;
-            } 
+            }
             else if (firmware.PreviousId != null)
             {
                 nextId = (int)firmware.PreviousId;
