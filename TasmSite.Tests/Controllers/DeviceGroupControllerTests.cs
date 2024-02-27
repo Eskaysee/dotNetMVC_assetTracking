23d1c634ed660a8845ab8908b979f47bc58b5810
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-02-27 02:52:45 +0200

    Tried implementing a running tests, to no success... Changed the Firmware details to show the device's catergories

---
+++ TasmSite.Tests/Controllers/DeviceGroupControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TasmSite.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace TasmSite.Controllers.Tests
+{
+    [TestClass()]
+    public class DeviceGroupControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            //Arrange
+            DeviceGroupController controller = new DeviceGroupController();
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+            Console.WriteLine(result.Model.ToString());
+            Console.WriteLine(result.ViewData.Keys.ToString());
+            Console.WriteLine(result.ViewData.Values.ToString());
+            Console.WriteLine(result.ViewBag.Message);
+            Console.WriteLine(result.ViewName);
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            //Arrange
+            DeviceGroupController controller = new DeviceGroupController();
+            //Act
+            ViewResult result = controller.Details() as ViewResult;
+            Debug.Write(result.Model.ToString());
+            Debug.Write(result.ViewData.Keys.ToString());
+            Debug.Write(result.ViewData.Values.ToString());
+            Debug.Write(result.ViewBag.Message);
+            Debug.Write(result.ViewName);
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            //Arrange
+            DeviceGroupController controller = new DeviceGroupController();
+            //Act
+            ViewResult result = controller.Create() as ViewResult;
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            //Arrange
+            DeviceGroupController controller = new DeviceGroupController();
+            //Act
+            ViewResult result = controller.Edit() as ViewResult;
+            //Assert
+
+        }
+    }
+}
---
+++ TasmSite.Tests/Controllers/FirmwareControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TasmSite.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace TasmSite.Controllers.Tests
+{
+    [TestClass()]
+    public class FirmwareControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            //Arrange
+            FirmwareController controller = new FirmwareController();
+            ViewResult result = controller.Index() as ViewResult;
+            //Act
+            //Assert
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            //Arrange
+            FirmwareController controller = new FirmwareController();
+            //Act
+            ViewResult result = controller.Details() as ViewResult;
+            //Assert
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            //Arrange
+            FirmwareController controller = new FirmwareController();
+            //Act
+            ViewResult result = controller.Create() as ViewResult;
+            //Assert
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            //Arrange
+            FirmwareController controller = new FirmwareController();
+            //Act
+            ViewResult result = controller.Edit() as ViewResult;
+            //Assert
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            //Arrange
+            FirmwareController controller = new FirmwareController();
+            //Act
+            ViewResult result = controller.Delete() as ViewResult;
+            //Assert
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConfirmedTest()
+        {
+            //Arrange
+            FirmwareController controller = new FirmwareController();
+            //Act
+            ViewResult result = controller.DeleteConfirmed() as ViewResult;
+            //Assert
+            Assert.Fail();
+        }
+    }
+}
